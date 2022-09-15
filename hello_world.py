9309c6c737ce7f86479b4f10e866977f3dd1cc11
Tyler Bell <Tyler.Bell@mths.ca>
2022-09-15 15:16:24 +0000

    init

---
+++ hello_world.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Created by: Tyler Bell
+# Created on: Sept 2022
+# This is the "Hello, World!" program, with proper style
+
+def main():
+    # this function prints to the console
+
+    print("Hello, World!")
+
+    print("\nDone.")
+
+if __name__ == "__main__":
+    main()
+
